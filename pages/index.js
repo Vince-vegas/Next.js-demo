b2ce0b1f6c47ddc7574a028193dc862dd8e89b52
vince vegas <sanchezvince769@gmail.com>
2020-12-04 19:55:44 +0800

    ssr

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { onIncrement } from '../store/reducer';
+import Link from 'next/link';
 
 export default function Home() {
   const number = useSelector((state) => state);
@@ -8,6 +9,9 @@ export default function Home() {
 
   return (
     <div className="container">
+      <Link href="/photos">
+        <a>to Photos</a>
+      </Link>
       <h1>Homepage</h1>
       <h1>Number: {number}</h1>
       <button onClick={() => dispatch(onIncrement())}>Increment</button>
